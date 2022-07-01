b92959adaead53ff4602d0362a6e898fe1be63c9
Hunter19823 <hspragg@asu.edu>
2022-06-30 22:32:00 -0700

    Friday Optional Lesson: Mob Behavior

--- src/main/java/com/idtech/entity/ZomboEntity.java
+++ src/main/java/com/idtech/entity/ZomboEntity.java
@@ -5,8 +5,15 @@ import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.MobCategory;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.entity.ai.goal.EatBlockGoal;
+import net.minecraft.world.entity.ai.goal.FloatGoal;
+import net.minecraft.world.entity.ai.goal.MeleeAttackGoal;
+import net.minecraft.world.entity.ai.goal.RandomLookAroundGoal;
+import net.minecraft.world.entity.ai.goal.target.HurtByTargetGoal;
+import net.minecraft.world.entity.ai.goal.target.NearestAttackableTargetGoal;
 import net.minecraft.world.entity.monster.Monster;
 import net.minecraft.world.entity.monster.Zombie;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.Level;
 
@@ -30,5 +37,15 @@ public class ZomboEntity extends Zombie {
                 .add(Attributes.ATTACK_DAMAGE, 3.0)
                 .add(Attributes.ARMOR, 2.00)
                 .add(Attributes.SPAWN_REINFORCEMENTS_CHANCE);
+    }
+
+    @Override
+    public void registerGoals() {
+        this.targetSelector.addGoal(0, new FloatGoal(this));
+        this.targetSelector.addGoal(1, new NearestAttackableTargetGoal<>(this, Player.class, false));
+        this.targetSelector.addGoal(3, new MeleeAttackGoal(this, 0.8f, false));
+        this.targetSelector.addGoal(4, (new HurtByTargetGoal(this)).setAlertOthers(ZomboEntity.class));
+        this.targetSelector.addGoal(8, new RandomLookAroundGoal(this));
+        this.targetSelector.addGoal(9, new EatBlockGoal(this));
     }
 }
